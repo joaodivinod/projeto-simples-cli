77cc6e2913b290e36988909b6c1f11538a0df136
João Divino <joaodivinod@gmail.com>
2020-11-06 13:13:30 -0300

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      {{contador}}
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    
+    data:function (){
+      return{
+        contador:1,
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app{
+    background-color: currentColor;
+    color: #fff;
+  }
 </style>
